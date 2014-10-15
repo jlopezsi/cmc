139f46756f1901877a575708f64a03878b4ad2da
User name <username@host>
2014-10-15 19:56:07 +0200

    r.station added

---
+++ r.station/0.project.config.R
@@ -0,0 +1,20 @@
+###########################################
+## File name: 
+## 
+## Change log:
+## 
+## ToDos:
+##
+###########################################
+
+prj.name <- "myproj"
+prj.saved.directory <- sprintf("%s.saved", prj.name)
+prj.data.directory <- sprintf("%s.data", prj.name)
+prj.csv.process.directory <- sprintf("%s.csv.process", prj.name)
+prj.graph.directory <- sprintf("%s.graph", prj.name)
+prj.graphic.directory <- sprintf("%s.graphic", prj.name)
+
+skip.session.debug <- F
+session.verbose.level <- 3
+session.debug.level <- 3
+session.log <- c("--")
