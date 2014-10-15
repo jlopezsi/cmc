25848f382a565e7c72dea8775564a188c83c0ade
User name <username@host>
2014-10-15 20:24:14 +0200

    t

---
+++ config.R
@@ -0,0 +1,13 @@
+prj.name <- "pp2"
+prj.saved.directory <- c("saved")
+prj.data.directory <- c("data")
+prj.csv.process.directory <- c("csv.process")
+prj.graph.directory <- c("graph")
+prj.graphic.directory <- c("graphic")
+prj.library.directory <- c("library")
+prj.archive.directory <- c("archive")
+prj.r.directory <- c("r")
+
+skip.session.debug <- F
+session.verbose.level <- 3
+session.debug.level <- 3
