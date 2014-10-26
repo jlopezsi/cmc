c7e0d51aad7a1dc55be9e1fa77b85e2d4f057fae
Abaghan Ghahraman <abaghan@cmc.uab.es>
2014-10-26 14:07:45 +0100

    cca.ouput

--- genre/r.gnr/03.cca.R
+++ genre/r.gnr/03.cca.R
@@ -9,16 +9,17 @@ load(pth.music.rdata)
 vec.genre <- grep("^glisten", colnames(df.music)) 
 
 df.genre <- df.music[vec.genre]
-tmp.genre  <- df.genre[1:100,]
+df.genre <- sapply(df.genre, as.numeric)
 
-df.cca <- tmp.genre
+df.cca <- df.genre
 cca.output <- cca(df.cca)
 summary(cca.output)
 cca.example.output
-plot(cca.example.output)
+plot(cca.output)
 library(RCA)
 RCA(cca.example)
 RCA.cca.example.output <- RCA(cca.example)
 summary(RCA.cca.example.output)
 table(RCA.cca.example.output$member)
 RCA.cca.example.output$merge
+
