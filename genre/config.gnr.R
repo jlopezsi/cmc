ba374da16e3a63fe59fb55b9dd20118027d5fc34
User name <username@host>
2014-10-28 14:26:41 +0100

    mca 20%

--- genre/config.gnr.R
+++ genre/config.gnr.R
@@ -2,6 +2,7 @@ prj.name <- "gnr"
 prj.import.state <- "done"
 prj.cca.state <- "done"
 prj.rca.state <- "done"
+prj.mca.state <- "done"
 prj.data.directory <- paste0("data",".",prj.name)
 prj.graph.directory <- paste0("graph",".",prj.name)
 prj.graphic.directory <- paste0("graphic",".",prj.name)
--- genre/r.gnr/00.requirements.R
+++ genre/r.gnr/00.requirements.R
@@ -2,4 +2,4 @@ source("../library.gnr/head.R")
 
 fun.install.require(c("Hmisc","rmarkdown","ggplot2","foreign","beepr"
                       , "circlize","xtable","stats", "Cairo"
-                      , "rmarkdown", "corclass", "RCA"))
+                      , "rmarkdown", "corclass", "RCA", "NMF", "ade4"))
