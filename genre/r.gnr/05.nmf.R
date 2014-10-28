1274718fbe4fa8a7a05d59679ca22853628758ce
jlopezsi <jordi.lopez@uab.es>
2014-10-28 15:13:13 +0100

    test

--- genre/r.gnr/05.nmf.R
+++ genre/r.gnr/05.nmf.R
@@ -3,9 +3,7 @@ source("../library.gnr/head.R")
 if(prj.nmf.state != "done"){
   # Onetime executives go here
 
-  # Load your classes here.
-  # I will add them to requirement afterwards
-  # fun.install.require(c(""))
+fun.install.require(c("NMF"))
 
   load("../data.gnr/music.Rdata")
 
@@ -13,7 +11,10 @@ if(prj.nmf.state != "done"){
   vec.genre <- seq(93, 153, by=3)
   df.genre <- df.music[vec.genre]
   df.genre <- sapply(df.genre, as.numeric)
-
+  nmf.fit <- nmf(df.genre, rank=3,  'lee')
+  coefmap(nmf.fit, tracks=NA)
+  basismap(nmf.fit, tracks=NA)
+  
   # Saving a sample variable
   save(nmf.var1, file="../data.gnr/nmf.var1.Rdata")
 }
@@ -21,6 +22,7 @@ if(prj.nmf.state != "done"){
 # Generate the report here
 rmarkdown::render(
                   "xx.05.nmf.Rmd"
-                  , output_file= "../report.gnr/04.nmf.pdf"
+                  , output_file= "../report.gnr/05.nmf.pdf"
                   );
 fun.beep(8) # Mario winning sound
+
