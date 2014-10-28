03f875e1bc37e07aa856f7703c51d59532a16af0
User name <username@host>
2014-10-28 19:26:08 +0100

    fixed the mca.cluster.output list problem

--- genre/r.gnr/04.mca.R
+++ genre/r.gnr/04.mca.R
@@ -1,6 +1,6 @@
 source("../library.gnr/head.R")
 
-if(prj.mca.state != "done"){
+if(prj.mca.state != "tmp.done"){
   # Loading libraries
   fun.install.require(c("ade4"))
 
@@ -23,7 +23,7 @@ if(prj.mca.state != "done"){
 
   mca.cluster.output = list()
   for(cnt.i in 1:length(levels(as.factor(cca.membership)))){
-    mca.cluster.output <- dudi.acm(df.genre[cca.membership == cnt.i, ], scannf=F,  nf=2)
+    mca.cluster.output[[cnt.i]] <- dudi.acm(df.genre[cca.membership == cnt.i, ], scannf=F,  nf=2)
   }
   save(mca.cluster.output, file="../data.gnr/mca.cluster.output.Rdata")
 
