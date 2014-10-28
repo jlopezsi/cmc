3609c9da0fca7f262fb513cae349d804fb3204c9
User name <username@host>
2014-10-28 19:01:56 +0100

    fixing cca zero action 100% done. moving to server

--- genre/r.gnr/04.mca.R
+++ genre/r.gnr/04.mca.R
@@ -6,14 +6,27 @@ if(prj.mca.state != "tmpdone"){
 
   # Loading music data
   load("../data.gnr/music.Rdata")
+  # Loading vector of rows with zero cluster in cca
+  load("../data.gnr/cca.zeros.Rdata")
+  # Loading vector of cca membership
+  load("../data.gnr/cca.membership.Rdata")
 
   # Creating genre dataframe
   vec.genre <- seq(93, 153, by=3)
   df.genre <- df.music[vec.genre]
+  # Excluding rows with zero cluster
+  df.genre <- df.genre[-cca.zeros, ]
 
+  # analyze and save the output of mca 
   mca.output <- dudi.acm(df.genre, scannf=F,  nf=2)
-
   save(mca.output, file="../data.gnr/mca.output.Rdata")
+
+  mca.cluster.output = list()
+  for(cnt.i in 1:length(levels(as.factor(cca.membership)))){
+    mca.cluster.output <- dudi.acm(df.genre[cca.membership == cnt.i, ], scannf=F,  nf=2)
+  }
+  save(mca.cluster.output, file="../data.gnr/mca.cluster.output.Rdata")
+
 }
 
 rmarkdown::render(
