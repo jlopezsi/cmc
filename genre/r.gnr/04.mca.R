8d82c649de636b43aaaf5023c849285fbb7afaa5
User name <username@host>
2014-10-30 19:06:19 +0100

    mca plotting observations

--- genre/r.gnr/04.mca.R
+++ genre/r.gnr/04.mca.R
@@ -1,8 +1,8 @@
 source("../library.gnr/head.R")
 
-if(prj.mca.state != "tmpdone"){
+if(prj.mca.state != "done"){
   # Loads libraries
-  fun.install.require(c("ca"))
+  fun.install.require(c("ca", "ade4"))
 
   # Loads music data
   load("../data.gnr/music.Rdata")
@@ -66,16 +66,16 @@ if(prj.mca.state != "tmpdone"){
   save(mca.ade4.output, file="../data.gnr/mca.ade4.output.Rdata")
 
   # A list containing all mca.ade4 subset results
-  mca.ade4.cluster.output = list()
+  mca.ade4.module.output = list()
 
   # for each cca module
   for(cnt.i in 1:length(levels(as.factor(cca.membership)))){
     # Conducts mca.ade4 for current cca mudlue
-    mca.ade4.cluster.output[[cnt.i]] <- dudi.acm(df.genre[cca.membership == cnt.i, ], scannf=F,  nf=2)
+    mca.ade4.module.output[[cnt.i]] <- dudi.acm(df.genre[cca.membership == cnt.i, ], scannf=F,  nf=2)
   }
 
   # Saves the list of cca.ade4 subset results for each module
-  save(mca.ade4.cluster.output, file="../data.gnr/mca.ade4.cluster.output.Rdata")
+  save(mca.ade4.module.output, file="../data.gnr/mca.ade4.module.output.Rdata")
 }
 
 # Renders the report
