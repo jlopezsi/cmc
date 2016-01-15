8462ce5cdedf3f15d6bdb6854139060a7a2218ef
Abaghan <abaghan@gmail.com>
2016-01-15 16:44:36 +0100

    fixing fun.viki.make.url.paging

--- tvseries/library.tvs/viki.R
+++ tvseries/library.tvs/viki.R
@@ -5,19 +5,19 @@ fun.viki.make.url <- function(url, api.id, timestamp){
   if(length(grep("\\?", url))==0){
     url <- paste0(url, "?")
   }
-  return(paste0(url, "app=", api.id, "&t=",timestamp))
+  timestamp <- fun.make.timestamp()
+  return(paste0(url, "app=", prj.viki.api.id, "&t=",timestamp))
 }
 
 ## makes the paging and returns the viki url 
 fun.viki.make.url.paging <- function(url, page.number, perpage){
-  paging.url <- fun.viki.make.url(url, prj.viki.api.id, timestamp)
+  paging.url <- fun.viki.make.url(url)
   paging <- paste0("page=",page.number,"&per_page=",perpage)
   return(paste0(paging.url,"&",paging))
 }
 
 ## makes the sig and returns the full viki url
 fun.viki.make.url.sig <- function(url, page.number, perpage){
-  timestamp <- fun.make.timestamp()
   viki.url <- fun.viki.make.url.paging(url, page.number, perpage)
   sig <- hmac(viki.url, prj.viki.api.secret, algo="sha1")
   viki.url.sig <- paste0(viki.url,"&sig=", sig)
@@ -32,13 +32,7 @@ fun.viki.make.domain.url.sig <- function(url, page.number, perpage){
 }
 
 ## downloads the viki url and saves `.json` and `.url` files
-fun.viki.download.url <- function(url, fil.name, page.number, perpage){
-  if(!page.number){
-    page.number <- 1
-  }
-  if(!perpage){
-     perpage <- 25
-  }
+fun.viki.download.url <- function(url, fil.name, page.number=1, perpage=25){
   domain.sig.url <- fun.viki.make.domain.url.sig(url, page.number, perpage)
   fun.verbose(domain.sig.url)
   fil.name <- paste0("../", prj.rawdata.directory,"/", fil.name)
--- tvseries/playground.tvs/03.get.list.R
+++ tvseries/playground.tvs/03.get.list.R
@@ -8,6 +8,7 @@ fun.install.require(c("digest","RJSONIO"))
 ## A list of on air containers : List id is 2l
 ## A list of upcoming containers : List id is 6l
 tmp.list.id <- "1l"
+tmp.page.number <- "1"
 
 ## Make URL with sig and download the file------------------------------------------- 
 #str.url <- paste0("/v4/lists/",tmp.list.id ,".json")
@@ -18,8 +19,8 @@ tmp.list.id <- "1l"
 
 #finding max pages-------------------------------------------------------------------- 
 
-str.url <- paste0("/v4/lists/",tmp.list.id ,".json") 
+str.url <- paste0("/v4/lists/", tmp.list.id , ".json") 
 
-fil.viki.list <- paste0("tvs.",page.number,".list")
+fil.viki.list <- paste0("tvs.", tmp.page.number, ".list")
 
-fun.viki.download.url(str.url, fil.viki.list)
+fun.viki.download.url(url=str.url, fil.name=fil.viki.list, page.number=tmp.page.number)
