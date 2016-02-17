3e8c7106186600e6b57f109c41b786e4aacfa419
Miajinju <miabea44@gmail.com>
2016-02-17 10:46:24 +0100

    clean data try to collect only meaningful english.

--- tvseries/playground.tvs/09.cleaning.data.R
+++ tvseries/playground.tvs/09.cleaning.data.R
@@ -23,12 +23,17 @@ data.29584c.01.en.json <- gsub("\\d+", "", data.29584c.01.en.json)
 # remove number
 data.29584c.01.en.json <- gsub("[[:digit:]]", "", data.29584c.01.en.json)
 # remove url
-data.29584c.01.en.json <- gsub("http\\+", "", data.29584c.01.en.json)
+data.29584c.01.en.json <- gsub("http+", "", data.29584c.01.en.json)
 # remove user information
 data.29584c.01.en.json <- gsub("[user:+]", "", data.29584c.01.en.json)
 #example of user information : "user":{"id":"7905008u","username":"minsu_lee","name":"MInsu Lee","staff":false,"images":{"avatar":{"url":"https://graph.facebook.com/100001765202612/picture?width=215&height=215"}}}},
 #
-data.29584c.01.en.json <- gsub("[[:digit:]]", "", data.29584c.01.en.json)
+fn <- tempfile()
+writeLines(data.29584c.01.en.json, fn)
+result <- aspell(fn)
+result$Original
+txt.data.29584c.01.en <- txt[!(txt %in% result$Original)]
+head(txt.data.29584c.01.en)
 ## define "tolower error handling" function
 try.error = function(x)
 {
@@ -44,16 +49,28 @@ return(y)
 }
 
 ## lower case using try.error with sapply
-data.29584c.01.en.json <- sapply(data.29584c.01.en.json, try.error)
+txt.data.29584c.01.en <- sapply(txt.data.29584c.01.en, try.error)
 
 ## remove NAs in some_txt
-data.29584c.01.en.json = data.29584c.01.en.json [!is.na(data.29584c.01.en.json)]
-names(data.29584c.01.en.json) = NULL
+txt.data.29584c.01.en = txt.data.29584c.01.enn [!is.na(txt.data.29584c.01.en)]
+names(txt.data.29584c.01.en) = NULL
 
-corpus <- data.29584c.01.en.json
+## make a corpus
+corpus <- txt.data.29584c.01.en
+# covert string to vector of words
+corpus1 <- unlist(strsplit(corpus, split = ","))
+# find indices of words with non-ASCII characters
+corpus2 <- grep("corpus1", iconv(corpus1, "latin1", "ASCII", sub = "corpus1"))
+# subset original vector or words to exclude words with non- ASCII chr
+corpus3 <- corpus1[-corpus2]
+# convert vector back to a string
+corpus4 <- paste(corpus3, collapse = ",")
+# make a corpus
+txt.corpus <- Corpus(VectorSource(corpus4))
+inspect(txt.corpus)
 
 ## words frequency
-dtm <- DocumentTermMatrix(corpus)
+dtm <- DocumentTermMatrix(txt.corpus)
 dtm2 <- as.matrix(dtm)
 frequency <- colSums(dtm2)
 frequency <- sort(frequency, decreasing=TRUE)
@@ -61,5 +78,5 @@ head(frequency)
 
 ## words could
 words <- names(frequency)
-wordcloud(words[1:500], frequency[1:500])
+wordcloud(words[1:100], frequency[1:100])
 
