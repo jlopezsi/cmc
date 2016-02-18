e402041beb7347bbc914454e6d6469f0641a6457
Miajinju <miabea44@gmail.com>
2016-02-18 08:22:43 +0100

    small change but no more touch today

--- tvseries/playground.tvs/09.cleaning.data.R
+++ tvseries/playground.tvs/09.cleaning.data.R
@@ -1,4 +1,6 @@
 ## Cleaning the data
+## ? only choose Korean drama files among the all the series ?
+
 ## Download a relevant package
 library(jsonlite)
 library(tm)
@@ -9,8 +11,18 @@ library(tm)
 
 ## Importing data from Json file
 data.29584c.01.en.json <- fromJSON("../rawdata.tvs/29584c/timed.comments.01.en.json")
+class(data.29584c.01.en.json)
 
 ## Prepare the text for analysis
+# remove number
+data.29584c.01.en.json <- gsub("[[:digit:]]", "", data.29584c.01.en.json)
+# remove url
+data.29584c.01.en.json <- gsub("http+", "", data.29584c.01.en.json)
+# remove .jpg , .png
+data.29584c.01.en.json <- gsub("+.jpg", "", data.29584c.01.en.json)
+data.29584c.01.en.json <- gsub("+.png", "", data.29584c.01.en.json)
+# remove user information
+data.29584c.01.en.json <- gsub("[user:+]", "", data.29584c.01.en.json)
 # remove unnecessary space
 data.29584c.01.en.json <- gsub("[ \t]{2,}","",data.29584c.01.en.json)
 data.29584c.01.en.json <- gsub("^\\s+|\\s+$", "", data.29584c.01.en.json)
@@ -20,20 +32,9 @@ data.29584c.01.en.json <- gsub("[[:punct:]]", "", data.29584c.01.en.json)
 data.29584c.01.en.json <- gsub("[[:cntrl:]]", "", data.29584c.01.en.json)
 # remove
 data.29584c.01.en.json <- gsub("\\d+", "", data.29584c.01.en.json)
-# remove number
-data.29584c.01.en.json <- gsub("[[:digit:]]", "", data.29584c.01.en.json)
-# remove url
-data.29584c.01.en.json <- gsub("http+", "", data.29584c.01.en.json)
-# remove user information
-data.29584c.01.en.json <- gsub("[user:+]", "", data.29584c.01.en.json)
-#example of user information : "user":{"id":"7905008u","username":"minsu_lee","name":"MInsu Lee","staff":false,"images":{"avatar":{"url":"https://graph.facebook.com/100001765202612/picture?width=215&height=215"}}}},
-#
-fn <- tempfile()
-writeLines(data.29584c.01.en.json, fn)
-result <- aspell(fn)
-result$Original
-txt.data.29584c.01.en <- txt[!(txt %in% result$Original)]
-head(txt.data.29584c.01.en)
+
+# example of user information : "user":{"id":"7905008u","username":"minsu_lee","name":"MInsu Lee","staff":false,"images":{"avatar":{"url":"https://graph.facebook.com/100001765202612/picture?width=215&height=215"}}}},
+
 ## define "tolower error handling" function
 try.error = function(x)
 {
@@ -41,7 +42,7 @@ try.error = function(x)
 y = NA
 # tryCatch error
 try_error = tryCatch(tolower(x), error=function(e) e)
-#if not an error
+# if not an error
 if (!inherits(try_error, "error"))
 y = tolower(x)
 # result
@@ -49,11 +50,23 @@ return(y)
 }
 
 ## lower case using try.error with sapply
-txt.data.29584c.01.en <- sapply(txt.data.29584c.01.en, try.error)
+data.29584c.01.en.json <- sapply(data.29584c.01.en.json, try.error)
 
 ## remove NAs in some_txt
-txt.data.29584c.01.en = txt.data.29584c.01.enn [!is.na(txt.data.29584c.01.en)]
-names(txt.data.29584c.01.en) = NULL
+data.29584c.01.en.json = data.29584c.01.en.json [!is.na(data.29584c.01.en.json)]
+names(data.29584c.01.en.json) = NULL
+
+txt.data.29584c.01.en <- data.29584c.01.en.json
+
+## only choose correct english words from among all (Aspell)
+## Aspell programme : http://ftp.gnu.org/gnu/aspell/w32/Aspell-0-50-3-3-Setup.exe
+## Aspell dictionary : http://ftp.gnu.org/gnu/aspell/w32/Aspell-en-0.50-2-3.exe
+#fn <- tempfile()
+#writeLines(data.29584c.01.en.json, fn)
+#result <- aspell(fn)
+# result$Original
+#txt.data.29584c.01.en <- txt[!(txt %in% result$Original)]
+#head(txt.data.29584c.01.en)
 
 ## make a corpus
 corpus <- txt.data.29584c.01.en
